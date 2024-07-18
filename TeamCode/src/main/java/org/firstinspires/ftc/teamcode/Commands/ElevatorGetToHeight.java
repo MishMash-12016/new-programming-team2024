5d739890ceb091fc8e2e1a9bbe838607d4c487a4
mishmash <nevoday2006@gmail.com>
2024-07-18 19:37:41 +0300

    Started Elevator subsystem, created the getHeight() method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ElevatorGetToHeight.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+public class ElevatorGetToHeight extends CommandBase {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Elevator.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Elevator extends SubsystemBase {
+    private final double TICKS_PER_REVOLUTION = 537.6;
+    private final double SPROCKET_DIAMETER = 2.59;
+    private final int NUM_LEVELS = 3;
+    private DcMotorEx motorRight;
+    private DcMotorEx motorLeft;
+    private PIDController pidController;
+
+    public Elevator(HardwareMap hardwareMap) {
+        this.motorRight = hardwareMap.get(DcMotorEx.class, "ElevatorRight");
+        this.motorLeft = hardwareMap.get(DcMotorEx.class, "ElevatorLeft");
+        motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //TODO: check which motor to reverse
+    }
+
+
+    public void setPower(double power) {
+        motorRight.setPower(power);
+        motorLeft.setPower(power);
+    }
+
+    public double getHeight() {
+        double revolution = motorRight.getCurrentPosition() / TICKS_PER_REVOLUTION; // this is how many revolutions have passed, rev - revolution.
+        double chainLengthenInRev = Math.PI * SPROCKET_DIAMETER; // peremeter
+        double chainLength = chainLengthenInRev * revolution;
+        return chainLength * NUM_LEVELS;
+    }
+
+}
